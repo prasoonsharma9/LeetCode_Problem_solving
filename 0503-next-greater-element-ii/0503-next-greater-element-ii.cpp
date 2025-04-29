f3967a6cb2ba5bb772cedaae3b8b43010bf42c0a
Prasoon Kumar Sharma <139127426+prasoonsharma9@users.noreply.github.com>
2025-04-29 16:43:02 +0530

    Time: 2 ms (80.65%), Space: 29.2 MB (14.1%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
@@ -3,17 +3,17 @@ public:
     vector<int> nextGreaterElements(vector<int>& nums) {
         int n = nums.size();
         vector<int> ans;
-        if(n==1) ans.push_back(-1);
-        for(int i=0; i<n; i++){
-            for(int j=i+1; j<=i+n-1; j++){
-                int idx = j%n;
-                if(nums[idx] > nums[i]){
-                    ans.push_back(nums[idx]);
-                    break;
-                }
-                if(idx == (i+n-1)%n) ans.push_back(-1);
+        stack<int> st;
+        for(int i=2*n-1; i>=0; i--){
+            while(!st.empty() && nums[i%n] >= st.top()) st.pop();
+
+            if(i < n){
+                if(st.empty()) ans.push_back(-1);
+                else ans.push_back(st.top());
             }
+            st.push(nums[i%n]);
         }
+        reverse(ans.begin(), ans.end()); 
         return ans;
     }
 };
