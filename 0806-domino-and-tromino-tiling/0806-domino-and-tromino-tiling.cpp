bafd389c7411804217984166bebab89ad531beca
Prasoon Kumar Sharma <139127426+prasoonsharma9@users.noreply.github.com>
2025-05-05 21:52:19 +0530

    Time: 0 ms (100%), Space: 8.8 MB (63.36%) - LeetHub

--- 0806-domino-and-tromino-tiling/0806-domino-and-tromino-tiling.cpp
+++ 0806-domino-and-tromino-tiling/0806-domino-and-tromino-tiling.cpp
@@ -1,16 +1,24 @@
 class Solution {
 public:
     int m = 1000000007;
-    int f(int n, vector<int>& dp){
-        if(n == 1 || n == 2) return n;
-        if(n == 3) return 5;
+    // int f(int n, vector<int>& dp){
+    //     if(n == 1 || n == 2) return n;
+    //     if(n == 3) return 5;
 
-        if(dp[n] != -1) return dp[n];
+    //     if(dp[n] != -1) return dp[n];
 
-        return dp[n] = (2*f(n-1, dp) % m + f(n-3, dp) % m) % m;
-    }
+    //     return dp[n] = (2*f(n-1, dp) % m + f(n-3, dp) % m) % m;
+    // }
+
     int numTilings(int n) {
-        vector<int> dp(n+1, -1);
-        return f(n, dp);
+        if(n == 1 || n == 2) return n;
+        vector<int> dp(n+1, 0);
+        dp[1] = 1;
+        dp[2] = 2;
+        dp[3] = 5;
+        for(int i=4; i<=n; i++){
+            dp[i] = (2*dp[i-1] % m + dp[i-3] % m) % m;
+        }
+        return dp[n];
     }
 };
