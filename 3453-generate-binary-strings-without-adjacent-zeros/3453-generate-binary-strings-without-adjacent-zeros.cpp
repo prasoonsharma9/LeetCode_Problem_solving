d7a33943118799d3fe64a8c6efa6b29bb7d9bcaa
Prasoon Kumar Sharma <139127426+prasoonsharma9@users.noreply.github.com>
2025-06-17 01:13:02 +0530

    Time: 9 ms (82.73%), Space: 20.8 MB (84.48%) - LeetHub

---
+++ 3453-generate-binary-strings-without-adjacent-zeros/3453-generate-binary-strings-without-adjacent-zeros.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    void func(int n, int len, vector<string>& ans, string &temp){
+        if(len == n){
+    ans.push_back(temp); // accept all valid strings
+    return;
+}
+
+        if(temp.empty() || temp.back() != '0'){
+            temp += '0';
+            func(n, len+1, ans, temp);
+            temp.pop_back();
+        }
+
+        temp += '1';
+        func(n, len+1, ans, temp);
+        temp.pop_back();
+    }
+
+    vector<string> validStrings(int n) {
+        vector<string> ans;
+        int len = 0;
+        string temp = "";
+        func(n, len, ans, temp);
+        return ans;
+    }
+};
