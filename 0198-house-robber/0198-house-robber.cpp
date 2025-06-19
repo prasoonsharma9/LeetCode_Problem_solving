030d75129625cca86b242e30ccae8d37bd3cd6df
Prasoon Kumar Sharma <139127426+prasoonsharma9@users.noreply.github.com>
2025-06-19 18:51:10 +0530

    Time: 0 ms (100%), Space: 10.9 MB (5.81%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,26 +1,27 @@
 class Solution {
 public:
-    int tryRob(int idx, vector<int>& nums, vector<int>& dp) {
-        int n = nums.size();
-        if(idx >= n){
-            return 0;
-        }
+//MEMOIZATION (TOP - DOWN) APPROACH
+    // int f(int idx, vector<int>& nums, vector<int>& dp){
+    //     if(idx == 0) return nums[idx];
+    //     if(idx < 0) return 0;
+    //     if(dp[idx] != -1) return dp[idx];
 
-        if(dp[idx] != -1) return dp[idx];
+    //     int pick = nums[idx] + f(idx-2, nums, dp);
+    //     int notPick = 0 + f(idx-1, nums, dp);
 
-        // Pick current house
-        int pick = nums[idx] + tryRob(idx + 2, nums, dp);
-
-        // Skip current house
-        int skip = tryRob(idx + 1, nums, dp);
-
-        // Store the max of both choices
-        return dp[idx] = max(pick, skip);
-    }
-
+    //     return dp[idx] = max(pick, notPick); 
+    // }
     int rob(vector<int>& nums) {
         int n = nums.size();
-        vector<int> dp(n, -1);
-        return tryRob(0, nums, dp);
+        vector<int>dp (n,-1);
+        dp[0] = nums[0];
+        for(int i=1; i<n; i++){ 
+            int pick = nums[i];
+            if(i > 1) pick += dp[i-2];
+            int no = 0 + dp[i-1];
+
+            dp[i] = max(pick, no);
+        }
+        return dp[n-1];
     }
 };
