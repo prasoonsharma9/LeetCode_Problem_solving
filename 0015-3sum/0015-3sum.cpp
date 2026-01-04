d1185f7ef9b15787973908e726a9af7371787404
Prasoon Kumar Sharma <139127426+prasoonsharma9@users.noreply.github.com>
2026-01-04 23:50:43 +0530

    Time: 102 ms (16.01%), Space: 36.2 MB (14.79%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -19,6 +19,8 @@ public:
                     st.insert({nums[i], nums[j], nums[k]});
                     j++;
                     k--;
+                    while(j < k && nums[j] == nums[j - 1]) j++;
+                    while(j < k && nums[k] == nums[k + 1]) k--;
                 }
             }
         }
